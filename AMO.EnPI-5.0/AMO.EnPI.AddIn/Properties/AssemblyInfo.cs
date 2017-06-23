71c3f6771863ad4a1ddabcd4bc4f4d091320771d
g9a <accawigk@ornl.gov>
2017-06-23 11:23:20 -0400

    Repair errors in build for .NET 4.61

--- AMO.EnPI-5.0/AMO.EnPI.AddIn/Properties/AssemblyInfo.cs
+++ AMO.EnPI-5.0/AMO.EnPI.AddIn/Properties/AssemblyInfo.cs
@@ -36,5 +36,3 @@ using Microsoft.Office.Tools.Excel;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.*")]
 //[assembly: AssemblyFileVersion("1.0.0.0")]
-
-[assembly: SecurityTransparent()]
